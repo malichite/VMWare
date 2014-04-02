2e9a5b04d9e4e06b5a17a2cb99457bb56febaef9
rlapoint <rlapoint@starbucks.net>
2014-04-02 10:16:06 -0700

    Add Quick Build and SetGuestIP

---
+++ QuickBuild.ps1
@@ -0,0 +1,10 @@
+﻿#vmbuild
+$vmname="server1","server2","server3","server4"
+$vmHosts = get-cluster vmCluster | get-vmhost
+$custom=get-OSCustomizationSpec customizationSpec
+$template=get-template templateName
+$datastore=get-vmhost someHost | get-datastore
+foreach ($vm in $vmname){
+    $vmhost=get-random -input $vmhosts
+    new-vm -name $vm -OScustomizationspec $custom -template $template -vmhost $vmhost -datastore $datastore -RunAsync -DiskStorageFormat Thin
+    }
---
+++ SetGuestIP.ps1
@@ -0,0 +1,21 @@
+﻿#Set VM Guest IP addresses
+Function Set-WinVMIP ($VM, $HC, $GC, $IP, $SNM, $GW){
+ $netsh = "c:\windows\system32\netsh.exe interface ip set address ""Local Area Connection 3"" static $IP $SNM $GW 1"
+ Write-Host "Setting IP address for $VM..."
+ Invoke-VMScript -VM $VM -HostCredential $HC -GuestCredential $GC -ScriptType bat -ScriptText $netsh
+ Write-Host "Setting IP address completed."
+}
+ 
+#Connect-VIServer MYvCenter
+ 
+
+$VM = Get-VM ( Read-Host "Enter VM name")
+$ESXHost = $VM | Get-VMHost
+$HostCred = $Host.UI.PromptForCredential("Please enter credentials", "Enter ESX host credentials for $ESXHost", "root", "")
+$GuestCred = $Host.UI.PromptForCredential("Please enter credentials", "Enter Guest credentials for $VM", "", "")
+ 
+$IP = read-host "IP"
+$SNM = "255.255.255.0"
+$GW = read-host "Gateway"
+ 
+Set-WinVMIP $VM $HostCred $GuestCred $IP $SNM $GW
